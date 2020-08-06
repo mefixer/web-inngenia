cea9f7b032f99da617f0e5a6740f934f51f65817
Mauricio E Garcia <mgarciamardones@gmail.com>
2020-08-06 15:46:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# web-inngenia
+ Web Inngenia
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css'
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <nav>
+      <div class="nav-wrapper grey lighten-5">
+        <a href="#" class="brand-logo blue-text text-darken-2">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down ">
+          <li><a href="sass.html" class="blue-text text-darken-2"></a></li>
+          <li><a href="badges.html" class="blue-text text-darken-2">Equipo</a></li>
+          <li><a href="collapsible.html" class="blue-text text-darken-2">Contactanos</a></li>
+        </ul>
+      </div>
+    </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
